7dca6a55d9b118e5226f1cd2fca772e82ee537fe
founder <founder@A043>
2016-09-09 17:12:51 +0800

    some changes

--- src/xyz/luxin/java/secourse/Lab1.java
+++ src/xyz/luxin/java/secourse/Lab1.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
+import java.util.TreeSet;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
@@ -18,69 +19,270 @@ class ExpressionException extends Exception
 	}
 } 
 
-
-abstract class Expression {
-	//Expression Abstract Class
-}
-
-
-class Operator extends Expression {
-	
-	public char op;
-	
-	public Operator(char op) throws ExpressionException {
-
-	}
-	
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-}
-
-
-class Monomial extends Expression implements Comparable<Monomial> {
+class Monomial implements Comparable<Monomial> {
 	
 	public int constVaule;
 	public int varNumber;
 	public TreeMap<String, Integer> varIndex;
 	public int monIndex;
 	
-	public Monomial(String expString) throws ExpressionException {
+	public Monomial(String expString, boolean isExtraNegative) throws ExpressionException {
+		
+		constVaule = 1;
+		varNumber = 0;
+		varIndex = new TreeMap<String, Integer>();
+		monIndex = 0;
+		
+		if (isExtraNegative) {
+			constVaule = -constVaule;
+		}
+		
+		String pFactor = "((\\d+\\^\\d+)|([a-zA-Z]+\\^\\d+)|(\\d+)|([a-zA-Z]+))";
+		String pMonomial = "(" + pFactor + "(\\s*(\\*)?\\s*" + pFactor + ")*)";
+		
+		Pattern p1 = Pattern.compile(pMonomial);
+		Matcher m1 = p1.matcher(expString);
+		
+		if (!m1.matches()) {
+			throw new ExpressionException("Format Error");
+		}
+		
+		
+		Pattern p2 = Pattern.compile(pFactor);
+		Matcher m2 = p2.matcher(expString);
+		
+		while (m2.find()) {
+			//m2.group(0)
+			//...
+		}
 		
 		return;
 	}
 	
-	public Monomial findDerivative(String var) throws ExpressionException {
+	public Monomial() {
+		// TODO Auto-generated constructor stub
+		varIndex = new TreeMap<String, Integer>();
+	}
+
+	public Monomial derivative() {
 		return null;
 	}
 	
-	public Monomial evaluation(Map<String, Integer> pairs) throws ExpressionException {
+	public Monomial simplify(TreeMap<String, Integer> pairs) {
 		return null;
 	}
 
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		return null;
+		
+		if (varIndex.isEmpty()) {
+			return String.valueOf(constVaule);
+		}
+		
+		String result = "";
+		
+		if (constVaule != 1) {
+			result += String.valueOf(constVaule);
+			result += "*";
+		}
+		
+		Iterator<Entry<String, Integer>> it = varIndex.entrySet().iterator();
+		while (it.hasNext()) {
+			Entry<String, Integer> entry = (Entry<String, Integer>)it.next();
+			String key = entry.getKey();
+			Integer value = entry.getValue();
+			if (value==1) {
+				result = result + key + "*";
+			} else {
+				result = result + key + "^" + String.valueOf(value) + "*";
+			}
+		}
+			
+		if ((result.toCharArray())[result.length()-1]=='*') {
+			result = result.substring(0, result.length()-1);
+		}
+		
+		return result;
 	}
 
 	@Override
 	public int compareTo(Monomial o) {
 		// TODO Auto-generated method stub
 		
+		if (this.monIndex > o.monIndex) {
+			return 1;
+		} else if (this.monIndex < o.monIndex) {
+			return -1;
+		} else {
+			if (this.varNumber > o.varNumber) {
+				return 1;
+			} else if (this.varNumber < o.varNumber) {
+				return -1;
+			} else {
+				Iterator<Entry<String, Integer>> it1 = this.varIndex.entrySet().iterator();
+				Iterator<Entry<String, Integer>> it2 = o.varIndex.entrySet().iterator();
+				while (it1.hasNext() && it2.hasNext()) {
+					Entry<String, Integer> entry1 = (Entry<String, Integer>)it1.next();
+					Entry<String, Integer> entry2 = (Entry<String, Integer>)it1.next();
+					String key1 = entry1.getKey();
+					String key2 = entry2.getKey();
+					if (key1.compareTo(key2) > 0) {
+						return 1;
+					} else if (key1.compareTo(key2) < 0) {
+						return -1;
+					} else {
+						continue;
+					}
+				}
+			}
+		}
+		
 		return 0;
 	}
 }
 
 
-class Polynomial extends Expression {
+class Polynomial {
+	
+	private TreeMap<Monomial, Integer> mMonos;
+	
+	public Polynomial() {
+		mMonos = new TreeMap<Monomial, Integer>();
+	}
+	
+	public Polynomial(TreeMap<Monomial, Integer> monos) {
+		mMonos = monos;
+	}
+	
+	public void expression(String expString)  throws ExpressionException {
+		
+		String pFactor = "((\\d+\\^\\d+)|([a-zA-Z]+\\^\\d+)|(\\d+)|([a-zA-Z]+))";
+		String pMonomial = "(" + pFactor + "(\\s*(\\*)?\\s*" + pFactor + ")*)";
+		String pPolynomial = "(\\s*(" + pMonomial + "(\\s*[\\+\\-]\\s*" + pMonomial + ")*)\\s*)";
+		
+		Pattern p = Pattern.compile(pPolynomial);
+		Matcher m = p.matcher(expString);	
+		
+		Pattern pOp = Pattern.compile("([\\+\\-])");
+		Matcher mOp = pOp.matcher(expString);
+		
+		if (!m.matches()) {
+			throw new ExpressionException("Format Error");
+		}
+		
+			
+		Pattern p1 = Pattern.compile(pMonomial);
+		Matcher m1 = p1.matcher(expString);
+		
+		if (m1.find()) {	
+			Monomial mono = new Monomial(m1.group(0), false);
+			
+			if (mMonos.containsKey(mono)) {
+				//Map中已存在的单项式与get参数的单项式不是一个对象，两者仅系数不同
+				Integer n = mMonos.get(mono);
+				mono.constVaule = mono.constVaule + n;
+				mMonos.replace(mono, mono.constVaule);
+			} else {
+				mMonos.put(mono, mono.constVaule);
+			}	
+		}
+			
+		while (m1.find() && mOp.find()) {
+
+			boolean isExtraNegative = false;
+			
+			if (mOp.group(0)=="-") {
+				isExtraNegative = true;
+			}
+			
+			Monomial mono = new Monomial(m1.group(0), isExtraNegative);
+			
+			if (mMonos.containsKey(mono)) {
+				//Map中已存在的单项式与get参数的单项式不是一个对象，两者仅系数不同
+				Integer n = mMonos.get(mono);
+				mono.constVaule = mono.constVaule + n;
+				mMonos.replace(mono, mono.constVaule);
+			} else {
+				mMonos.put(mono, mono.constVaule);
+			}	
+		}
+		
+		return;
+	}
+	
+	public Polynomial simplify(TreeMap<Monomial, Integer> pairs) {
+		return null;
+		
+	}
+	
+	public Polynomial derivative() {
+
+		TreeMap<Monomial, Integer> result = new TreeMap<Monomial, Integer>();
+		
+		Iterator<Entry<Monomial, Integer>> it = mMonos.entrySet().iterator();
+		while (it.hasNext()) {
+			Entry<Monomial, Integer> entry = (Entry<Monomial, Integer>)it.next();
+			Monomial m = entry.getKey().derivative();
+			
+			if (result.containsKey(m)) {
+				//Map中已存在的单项式与get参数的单项式不是一个对象，两者仅系数不同
+				Integer n = result.get(m);
+				m.constVaule = m.constVaule + n;
+				result.replace(m, m.constVaule);
+			} else {
+				result.put(m, m.constVaule);
+			}
+		}
+
+		return new Polynomial(result);
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
 	
 }
 
 
 public class Lab1 {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Monomial m = new Monomial();
+		m.constVaule = -23;
+		m.varNumber = 2;
+		m.varIndex.put("z", 1);
+		m.varIndex.put("x", 1);
+		System.out.println(m);
+	}
+	
+	public static void testTreeMap() {
+		
+		TreeMap<String, Integer> tree = new TreeMap<String, Integer>();
+		
+		tree.put("xdf", 23);
+		tree.put("a", 4);
+		tree.put("zht", -9);
+		tree.put("zha", 56);
+		tree.put("abc", 1);
+		
+		Iterator<Entry<String, Integer>> it = tree.entrySet().iterator();
+		while (it.hasNext()) {
+			Entry<String, Integer> entry = (Entry<String, Integer>)it.next();
+			String key = entry.getKey();
+			Integer value = entry.getValue();
+			System.out.print(key + "  ");
+		}
+		System.out.println();
+		
+		return;
+	}
 	
 	public static void reference() {
 		
@@ -98,7 +300,7 @@ public class Lab1 {
 //		String pMonomial = "(\\s*(" + pFactor + "((\\*)?" + pFactor + ")*)\\s*)";
 //		String pPolynomial = "(\\s*(" + pMonomial + "([\\+\\-]" + pMonomial + ")*)\\s*)";
 		
-		String str = "2- 3 x*y +6*x^2y^4*z-y 	y*y	- z^7 ";
+		String str = "2- 3 z*x*y +6*x^2y^4*z-y 	y*y	- z^7 ";
 		String pFactor = "((\\d+\\^\\d+)|([a-zA-Z]+\\^\\d+)|(\\d+)|([a-zA-Z]+))";
 		String pMonomial = "(" + pFactor + "(\\s*(\\*)?\\s*" + pFactor + ")*)";
 		String pPolynomial = "(\\s*(" + pMonomial + "(\\s*[\\+\\-]\\s*" + pMonomial + ")*)\\s*)";
@@ -150,11 +352,6 @@ public class Lab1 {
 		} else {
 			System.out.println("Format Error");
 		}
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		reference();
 	}
 
 }
